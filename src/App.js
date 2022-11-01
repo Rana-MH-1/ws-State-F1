b87996bce25082653631476e8462b7c2b77c0618
ACER <Rana-MH-1>
2022-11-01 18:03:28 +0100

    ws state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Timer from "./Componenets/Timer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      name:'Wael',
+      count: 0,
+      val : null,
+      show:false
+    }
+  }
+
+Incerement(){
+  this.setState({count: this.state.count + 1})
+  }
+  Decrement(){
+    if(this.state.count>0){
+      this.setState({count: this.state.count- 1})
+    }
+  }
+  RESET(){
+    this.setState({count:0})
+  }
+
+  Increment_with_value(val){
+    this.setState({count: this.state.count + val})
+  }
+
+  render(){
+    let count = this.state.count
+    return (
+      <div className="App">
+       <h1>This is a ws of state -- class</h1>
+       <h3>{this.state.name}</h3>
+       <h2>{count}</h2>
+       <button onClick={()=>this.Incerement()}>INCREMENT</button>
+       <button onClick={()=> this.Decrement()}>DECREMENT</button>
+       <button onClick={()=> this.RESET()}>RESET</button>
+       <input onChange={(e)=>this.setState({val: +e.target.value})} placeholder="put a number" />
+       <button onClick={(e)=> this.Increment_with_value(this.state.val)}>INCREMENT With VALUE</button>
+       <hr/>
+       <button onClick={()=>this.setState({show:!this.state.show})}>Show Timer</button>
+      {this.state.show && <Timer/>}
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
